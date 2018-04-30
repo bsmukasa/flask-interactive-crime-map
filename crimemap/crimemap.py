f54c60531268e822b1728136bb36a7e0bf957cd2
Benjamin Mukasa <bsmukasa@gmail.com>
2018-04-30 13:58:23 -0400

    add error messages

--- crimemap/crimemap.py
+++ crimemap/crimemap.py
@@ -1,30 +1,35 @@
 import json
 
-from dbhelper import DBHelper
 from flask import Flask
 from flask import render_template
 from flask import request
+
+from dbhelper import DBHelper
 
 app = Flask(__name__)
 DB = DBHelper()
 
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes, error_message=error_message)
 
 
 @app.route("/submitcrime", methods=["POST"])
 def submit_crime():
-    category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
-    DB.add_crime(category, date, latitude, longitude, description)
-    return home()
+    try:
+        error_message = None
+        category = request.form.get("category")
+        date = request.form.get("date")
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+        description = request.form.get("description")
+        DB.add_crime(category, date, latitude, longitude, description)
+        return home(error_message=error_message)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
